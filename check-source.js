e266754f4686285d2b2a0b85ef59a2b72876b137
Tthfyth <thfythbhn@163.com>
2025-12-08 17:17:10 +0800

    删除无用文件

--- check-source.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('shareBookSource(1).json', 'utf8'));
-const idx = parseInt(process.argv[2] || '22');
-console.log(`书源 ${idx}:`, data[idx].bookSourceName);
-console.log('ruleBookInfo:', JSON.stringify(data[idx].ruleBookInfo, null, 2));
-console.log('ruleToc:', JSON.stringify(data[idx].ruleToc, null, 2));
